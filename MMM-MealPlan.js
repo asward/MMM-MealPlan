c1229237198e6130ae612de7d8456c45d1d38437
AW <asward@ucdavis.edu>
2019-06-30 18:36:55 +0100

    added readme and images

--- MMM-MealPlan.js
+++ MMM-MealPlan.js
@@ -1,6 +1,10 @@
 Module.register("MMM-MealPlan",{
     index: 0,
     meal_plan: [],
+    defaults:{
+        num_days: 7,
+        generateMeals: false ,
+    },
 
     //SELECTS and lists your meals for the week! Enter list of all meals you regularly prepare. Browse
     //to {server}/mealplan and select "New Plan" to generate a new list. The meals for the following week
@@ -13,23 +17,13 @@ Module.register("MMM-MealPlan",{
         return currentMeals; 
     },
     
-    // updateMeals: function(){
-    //     var self = this ;
-    //     var filteredAnnouncements = self.getFilteredAnnouncements() ;
-    //     var maxIndex = filteredAnnouncements.length-1 ;
-
-    //     if(++self.index > maxIndex){
-    //         self.index = 0 ;
-    //     }
-    //     self.current_announcement = filteredAnnouncements[self.index] ;
-    // },
     getHeader: function() {
 		return this.data.header ;
-	},
+    },
+    
 	// Override dom generator.
 	getDom: function() {
         var self = this ;
-        console.log(self) ;
 
         if (self.meal_plan){
             var wrapper = document.createElement("div");
@@ -54,20 +48,22 @@ Module.register("MMM-MealPlan",{
     },
     start: function() {
         var self = this ;
-        this.config.initialized = false ;
-        this.sendSocketNotification('GET_MEAL_PLAN', "");
+
+        this.sendSocketNotification('SET_CONFIG', this.config);
+        
         Log.log(this.name + ' is started!');        
 
         self.updateDom(0);
 
-        var now = new Date();
-        var millisTillMidNight = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0) - now;
-        if (millisTillMidNight< 0) {
-            millisTillMidNight += 24*60*60; // it's after 10am, try 10am tomorrow.
-        }
-        setTimeout(function(){
-            self.sendSocketNotification('GET_MEAL_PLAN', "");
-        }, millisTillMidNight);
+        this.getMealPlan();
+        setInterval(()=>{
+            this.getMealPlan();
+        },60*60*1000);
+        
+    },
+    
+    getMealPlan: function(){
+        this.sendSocketNotification('GET_MEAL_PLAN', {start_date:moment().format('YYYYMMDD'),num_days:this.config.num_days});
     },
 
     socketNotificationReceived:function(notification, payload) {
@@ -78,7 +74,7 @@ Module.register("MMM-MealPlan",{
             self.meal_plan = payload;
             self.updateDom(0);
         }  else if(notification = "MEAL_PLAN_UPDATED"){
-            self.sendSocketNotification('GET_MEAL_PLAN', "");
+            self.getMealPlan() ;      
         }
     },
     
--- node_helper.js
+++ node_helper.js
@@ -10,7 +10,7 @@ const loki = require('lokijs');
 const moment = require('moment');
 
 Module = {
-	configDefaults: {},
+	configDefaults: {num_days: 7},
 	register: function (name, moduleDefinition) {
 		// console.log("Module config loaded: " + name);
 		Module.configDefaults[name] = moduleDefinition.defaults;
@@ -37,100 +37,114 @@ module.exports = NodeHelper.create({
         
         this.createRoutes();    
     },
+
     databaseInitialize: function() {
         var self= this ;
 
         self.meals = self.db.getCollection('meals'); 
-        self.planned_meals = self.db.getCollection('planned_meals');
+        self.plans = self.db.getCollection('plans');
 
         if (self.meals === null) {
             self.meals = self.db.addCollection("meals");
         }
 
-        if (self.planned_meals === null) {
-            self.planned_meals = self.db.addCollection("planned_meals");
+        if (self.plans === null) {
+            self.plans = self.db.addCollection("plans");
         }
 
-        self.db.saveDatabase() ;
-        
+        self.db.saveDatabase() ;  
     },
+
     socketNotificationReceived: function(notification, payload) {
         var self = this ;
         console.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
 
-        if (notification == "GET_MEAL_PLAN") {
-
+        if (notification == "GET_MEAL_PLAN"){
             console.log(this.name + " Sending Meal Plan ");
-            self.sendSocketNotification('UPDATE_MEAL_PLAN', self.getMealPlan(payload.days_ahead || 7));
+            
+            self.sendSocketNotification('UPDATE_MEAL_PLAN', self.getMealPlan(payload.start_date,payload.num_days,false));
         } 
+        else if (notification === "SET_CONFIG")
+        {
+            this.config = payload;
+            console.log(this.config);
+        }  
     },
 
-    getMealPlan: function(days_ahead = 7){
+    getRandomMeal: function(){ 
+        // RANDOMLY PICK A MEAL
+        var meal_ids = this.meals.where(x=>true).map(m=>m.$loki);
+        var random_meal_id_index = Math.floor(Math.random() *  meal_ids.length);
+        return self.meals.findOne( { '$loki': { '$eq' :meal_ids[random_meal_id_index] } });
+    },
+    
+    getMealPlan: function(start_date,num_days,auto_generate){
+        
         var self = this ;
         //GETS days_ahead FROM TODAYS DATE OF MEALS
         //DB STORES ID FOR THE MEAL
         //METHOD RETURNS THE NAME OF THE MEAL
         //IF DB DOESN"T CONTAIN DATA FOR THOSE DATES IT'S GENERATED RANDOMLY
         var meal_plan = [];
-
-        for(i=0;i<days_ahead;i++){
-            // console.log(i) ;
-            var planned_meal = {} ;
+        for(i=0;i<num_days;i++){
             //GET EACH DAY PLANNED
-            var searchDate =  moment().add(i,'day').format('YYYYMMDD') ;
-            var plan = self.planned_meals.findOne( { date: { '$eq' :searchDate } } ) ;
+            var searchDate =  moment(start_date).add(i,'day').format('YYYYMMDD') ;
             
-            //IF NO DATA, RANDOMLY PICK A MEAL, ADD TO DB FOR THE DAY AND PUSH INTO MEAL PLAN
+            //PULL THE PLAN
+            var plan = self.plans.findOne( { date: { '$eq' :searchDate } } ) ;
+            var meal = null ;
+
             if(plan){
-                planned_meal = self.meals.findOne( { '$loki': { '$eq' :plan .id } });
-            }else{
-                var meal_ids = this.meals.where(x=>true);
-                // console.log(meal_ids);  
-                if(!meal_ids.length){
-                    //NO MEALS IN DB
-                    return [];
-                } else {       
-                    var num_ids = meal_ids.length ;
-                    //Find random meal
-                    var meal_id_index = Math.floor(Math.random() *  num_ids);
-                    
-                    planned_meal  = self.meals.findOne( { '$loki': { '$eq' :meal_ids.map(x=>x.$loki)[meal_id_index] } });
-
-                    plan ={id:planned_meal .$loki, 
-                        date:searchDate};
-
-                    //Add to meal_plan db
-                    self.planned_meals.insert(plan) ;
+                //FIND THE MEAL
+                meal = self.meals.findOne( { '$loki': { '$eq' :plan.meal_id } })  ;
+            }  
+            
+            if (!meal){
+                if(auto_generate){
+                    //IF ALLOWED TO GENERATE MEALS, PICK A RANDOM ONE AND INSTER TO PLANNED_MEALS
+                    meal = self.getRandomMeal() ;
+                    plan = {meal_id:meal.$loki,date:searchDate} ;
+                    self.plans.insert(plan) ;
+                }  else {
+                    //IF NO PLAN, AND NOT GENERATING MEALS CREATE AN 'EMPTY' MEAL
+                    meal = {
+                        name:'???'
+                    };
                 }
             }
             
-            meal_plan.push(
-                {
-                    name:planned_meal.name,
-                    date: plan.date,
-                    display_date: moment(plan.date).format('ddd, MMM DD')
-                }) ;
+            //PUSH INTO MEAL_PLAN ARRY
+            meal_plan.push({
+                name: meal.name,
+                date: searchDate,
+                display_date: moment(searchDate).format('ddd, MMM DD')
+            }) ;
 
         }
-        // console.log(meal_plan) ;
         return meal_plan ;            
     },
 
-    getMeals: function(){
-        var self = this ;
-        //GETS days_ahead FROM TODAYS DATE OF MEALS
-        //IF DB DOESN"T CONTAIN DATA FOR THOSE DATES IT'S GENERATED RANDOMLY
-        // console.log(self.meals.chain().where(x => true).data()); 
-        var meals =  self.meals.chain().where(x => true).data().map((m)=>(
-            {
-                name:m.name,
-                id:m.$loki
-            }
-        )) ;           
-        // console.log(meals);
-        return meals;
+    getAllMeals: function(){
+        var meals =  this.meals.chain().data().sort((a,b)=>{return a.name.localeCompare(b.name);}).map((m)=>{
+            m.id = m.$loki;
+            return m;
+        });     
+        return meals ;
     },
+    createNewMeal: function(meal_name){
+        var self = this ;
+        new_meal = {} ;
 
+        new_meal.name = meal_name ;
+
+        // console.log(new_meal.name);
+
+        //ADD TO DB
+        self.meals.insert(new_meal) 
+        self.db.save() ;
+
+        return  self.meals.findOne( { name: { '$eq' : meal_name} } ) ;
+    },
     createRoutes: function(){
         var self = this ;
 
@@ -138,41 +152,58 @@ module.exports = NodeHelper.create({
         ///MAIN- MEALPLAN
         self.expressApp.route('/mealplan')
         .get(function (req, res) {
-            var currentMealPlanListItems = self.renderListTemplate('/public/mealplan/mealplan_list_item.html', self.getMealPlan());
-            var currentMealPlanList = self.renderTemplate('/public/mealplan/mealplan_list.html', {meal_list:currentMealPlanListItems});
+            var meal_plan_index = self.renderTemplate('/public/mealplan/mealplan_index.html', {});
             var buttons = self.renderTemplate('/public/view_meals_btn.html', {});
         
             var template =  self.renderTemplate('/public/template.html',
             {
-                content: currentMealPlanList,
+                content:  meal_plan_index,
                 buttons:  buttons
             }) ;
             res.send(template); 
+        });
+
+        
+        ///MAIN- MEALPLAN
+        self.expressApp.route('/partial/mealplan/:date_start/:num_days')
+        .get(function (req, res) {
+
+            var date_start = req.params.date_start ;
+            var num_days = req.params.num_days ;
+
+            var currentMealPlanListItems = self.renderListTemplate('/public/mealplan/mealplan_list_item.html', self.getMealPlan(date_start,num_days));
+            var currentMealPlanList = self.renderTemplate('/public/mealplan/mealplan_list.html', {meal_list:currentMealPlanListItems});
+        
+            res.send(new Buffer(currentMealPlanList)); 
         });
 
 
         ///EDIT PLANNED MEAL - GET
         self.expressApp.get('/mealplan/edit/:date_id', self.textParser, function (req, res) {
             //EDIT SINGLE MEAL
-            var plan = self.planned_meals.findOne( { date: { '$eq' : req.params.date_id} } ) ;
-            var planned_meal = self.meals.findOne( { "$loki": { '$eq' :plan.id} } ) ;
-            if(planned_meal){
-                var available_meals = self.meals.chain().data({removeMeta:true});
-                var meal_name_option_list = self.renderListTemplate('/public/option_list.html',available_meals.map((m) => ({'data': m.name})));
-                var template = self.renderTemplate('/public/planned_meal/edit_planned_meal.html',
-                {
-                    meal_name:planned_meal.name,
-                    meal_date:moment(plan.date).format('dddd, MMMM Do'),
-                    meal_name_list: meal_name_option_list,
+            var meal_name_option_list = self.renderListTemplate('/public/option_list.html',self.getAllMeals().map((m) => ({'data': m.name})));
 
-                });
-                template = self.renderTemplate('/public/template.html',{content: template});
-
-                res.send(template); 
-            } else {
-                console.log('404') ;
-                res.send(self.get404()); 
+            var plan = self.plans.findOne( { date: { '$eq' : req.params.date_id} } ) ;
+            if(!plan){
+                plan = { meal_id:null,date:req.params.date_id }
             }
+
+            var meal = self.meals.findOne( { "$loki": { '$eq' :plan.meal_id} } ) ;    
+            if(!meal){
+                meal = {name:''}
+            }                
+                
+            var template = self.renderTemplate('/public/planned_meal/edit_planned_meal.html',
+            {
+                meal_name:meal.name,
+                meal_date:moment(plan.date).format('dddd, MMMM Do'),
+                meal_name_list: meal_name_option_list,
+
+            });
+            template = self.renderTemplate('/public/template.html',{content: template});
+
+            res.send(template); 
+
         });
         
         ///EDIT PLANNED MEAL - POST
@@ -186,22 +217,21 @@ module.exports = NodeHelper.create({
                 return res.sendStatus(400)
             } 
 
-            var planned_meal = self.planned_meals.findOne( { date: { '$eq' : req.params.date_id} } ) ;
-            var new_meal  = self.meals.findOne( { name: { '$eq' : req.body.mealName} } ) ;
-            console.log( planned_meal);
-            console.log(new_meal);
-            if(new_meal && planned_meal) {
-                if(req.body.action == "Update"){
-                    console.log('updateing to $loki ', new_meal.$loki);
-                    planned_meal.id = new_meal.$loki ;
-                    self.planned_meals.update(planned_meal);
-    
-                } else if (req.body.action == "Delete"){
-                    self.planned_meals.remove(planned_meal);
-                }
+            var meal  = self.meals.findOne( { name: { '$eq' : req.body.mealName} } ) ;
+            if(!meal){
+                meal = self.createNewMeal(req.body.mealName);
             }
 
-            self.sendSocketNotification('MEAL_PLAN_UPDATED', {});
+            var plan = self.plans.findOne( { date: { '$eq' : req.params.date_id} } ) ;
+            if(!plan) {
+                plan = {meal_id:meal.$loki,date:req.params.date_id}    
+                self.plans.insert(plan);
+            }
+
+            plan.meal_id = meal.$loki ;
+            self.plans.update(plan);
+
+            self.sendSocketNotification('MEAL_PLAN_UPDATED', {});    
 
             res.redirect('/mealplan');
         });
@@ -211,7 +241,7 @@ module.exports = NodeHelper.create({
         ///MAIN - MEALLIST
         self.expressApp.route('/mealplan/meallist')
         .get(function (req, res) {
-            var currentMealListItems = self.renderListTemplate('/public/meal/meal_list_item.html', self.getMeals());
+            var currentMealListItems = self.renderListTemplate('/public/meal/meal_list_item.html', self.getAllMeals());
             var currentMealList = self.renderTemplate('/public/meal/meal_list.html', {meal_list:currentMealListItems});
             var buttons = self.renderTemplate('/public/create_meal_btn.html', {});
             
@@ -237,19 +267,7 @@ module.exports = NodeHelper.create({
             //VALIDATE FORM
 
             //IF FORM IS VALID
-
-            //AUTO INCREMENT
-            new_meal = {} ;
-
-            new_meal.name = req.body.name ;
-
-            // console.log(new_meal.name);
-
-            //ADD TO DB
-            self.meals.insert(new_meal) 
-            self.db.save() ;
-
-            
+            self.createNewMeal(req.body.name);           
 
             res.redirect('/mealplan');
             //ELSE
@@ -267,7 +285,7 @@ module.exports = NodeHelper.create({
         });
 
         ///EDIT MEAL - GET        
-        self.expressApp.get('/mealplan/meallist/edit/:id', self.textParser, function (req, res) {
+        self.expressApp.get('/mealplan/meallist/edit/:meal_id', self.textParser, function (req, res) {
             //GET CREATE MEAL
             //FIND EXISTING MEAL
 
@@ -276,7 +294,7 @@ module.exports = NodeHelper.create({
             // console.log(self.meals) ;
             // var existingMeal = self.meals.chain().data({removeMeta:true})[0];
 
-            var existingMeal = self.meals.findOne( { '$loki': { '$eq' : parseInt(req.params.id) } });
+            var existingMeal = self.meals.findOne( { '$loki': { '$eq' : parseInt(req.params.meal_id) } });
             // console.log(existingMeal);
             if(existingMeal){
 
@@ -290,7 +308,7 @@ module.exports = NodeHelper.create({
         });
 
         ///EDIT MEAL - POST        
-        self.expressApp.post('/mealplan/meallist/edit/:id',  bodyParser.urlencoded({
+        self.expressApp.post('/mealplan/meallist/edit/:meal_id',  bodyParser.urlencoded({
             extended: false
         }), function (req, res) {
             //POST EDIT MEAL
@@ -301,19 +319,21 @@ module.exports = NodeHelper.create({
                 return res.sendStatus(400)
             } 
 
-            var existingMeal = self.meals.findOne( { "$loki": { '$eq' :parseInt(req.params.id) } }) ;
+            var existingMeal = self.meals.findOne( { "$loki": { '$eq' :parseInt(req.params.meal_id) } }) ;
             // console.log(existingMeal) ;
             if(existingMeal){
 
                 //VALIDATE FORM
 
-                //IF FORM IS VALID
-                existingMeal.name = req.body.name
-                self.meals.update(existingMeal);
+                //IF FORM IS VALID                
+                if(req.body.action == "Update"){
+                    existingMeal.name = req.body.name
+                    self.meals.update(existingMeal);
+                } else if (req.body.action == "Delete"){
+                    self.meals.remove(existingMeal);
+                }
 
                 self.sendSocketNotification('MEAL_PLAN_UPDATED', {});
-
-                
             }
 
             res.redirect('/mealplan/meallist');
